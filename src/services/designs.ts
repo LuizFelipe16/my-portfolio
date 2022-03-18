a048f12be20e5c0c340044e556ab72528c1a0ceb
Luiz Felipe <felipefelizatti215@gmail.com>
2022-03-18 15:18:04 -0300

    portfolio design

--- src/services/designs.ts
+++ src/services/designs.ts
@@ -10,4 +10,14 @@ export const designs: Design[] = [
     title: 'Criativo Disk Pizza',
     banner: 'disk-pizza.png',
   },
+  {
+    id: 2,
+    title: 'Criativo Corte de Cabelo',
+    banner: 'corte-cabelo.png',
+  },
+  {
+    id: 3,
+    title: 'Criativo Combo de Lanche',
+    banner: 'combo-lanche.png',
+  },
 ];
